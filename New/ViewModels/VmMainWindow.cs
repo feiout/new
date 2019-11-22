e6363ef38a4c5270650e743e44b0560f8e74f129
feiout <52486846@qq.com>
2019-11-22 16:00:22 +0800

    L2R C# client

---
+++ New/ViewModels/VmMainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using New.Common;
+using New.Entity;
+using New.RestUtility;
+using New.Service;
+
+
+namespace New.ViewModels
+{
+    public class VmMainWindow : ViewModelBase
+    {
+
+
+        public VmMainWindow()
+        {
+            
+
+        }
+
+
+
+
+
+
+    }
+}
